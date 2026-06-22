79e4743fc847ec8eb03c985c50b9ea9c4d2e6caf
Sven Wappler <wappler@wappler.systems>
2026-06-23 01:02:01 +0200

    14.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,15 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Wappler',
     'author_email' => 'typo3@wappler.systems',
     'state' => 'alpha',
-    'version' => '0.1.0',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '14.0.0-14.99.99',
             'php' => '8.2.0-8.99.99',
         ],
         'conflicts' => [],
-        'suggests' => [
-            'news' => '',
-        ],
     ],
 ];
